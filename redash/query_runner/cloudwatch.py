6542a638aa22b863e33d8ecdb72e8d5b7b72f0f7
Takaaki Abe <abnoumaru@gmail.com>
2024-04-15 09:34:09 +0900

    Add Assume Role in Cloudwatch data source

--- redash/query_runner/cloudwatch.py
+++ redash/query_runner/cloudwatch.py
@@ -1,8 +1,11 @@
 import datetime
+import logging
+import os
 
 import yaml
 
 from redash.query_runner import BaseQueryRunner, register
+from redash.settings import parse_boolean
 from redash.utils import parse_human_time
 
 try:
@@ -12,6 +15,8 @@ try:
 except ImportError:
     enabled = False
 
+logger = logging.getLogger(__name__)
+ASSUME_ROLE = parse_boolean(os.environ.get('ATHENA_ASSUME_ROLE', 'false'))
 
 def parse_response(results):
     columns = [
@@ -58,17 +63,51 @@ class CloudWatch(BaseQueryRunner):
 
     @classmethod
     def configuration_schema(cls):
-        return {
+        schema = {
             "type": "object",
             "properties": {
-                "region": {"type": "string", "title": "AWS Region"},
-                "aws_access_key": {"type": "string", "title": "AWS Access Key"},
-                "aws_secret_key": {"type": "string", "title": "AWS Secret Key"},
+                "region": {
+                    "type": "string",
+                    "title": "AWS Region"
+                },
+                "aws_access_key": {
+                    "type": "string",
+                    "title": "AWS Access Key"
+                },
+                "aws_secret_key": {
+                    "type": "string",
+                    "title": "AWS Secret Key"
+                },
             },
             "required": ["region", "aws_access_key", "aws_secret_key"],
             "order": ["region", "aws_access_key", "aws_secret_key"],
             "secret": ["aws_secret_key"],
         }
+
+        if ASSUME_ROLE:
+            del schema["properties"]["aws_access_key"]
+            del schema["properties"]["aws_secret_key"]
+
+            schema["required"].remove("aws_access_key")
+            schema["required"].remove("aws_secret_key")
+
+            schema["order"].remove("aws_access_key")
+            schema["order"].remove("aws_secret_key")
+
+            schema["properties"].update({
+                "assume_role_arn": {
+                    "type": "string",
+                    "title": "Assume Role ARN"
+                },
+                "external_id": {
+                    "type": "string",
+                    "title": "External ID"
+                },
+            })
+            schema["required"].extend(["assume_role_arn", "assume_role_session_name"])
+            schema["order"].extend(["region","assume_role_arn", "assume_role_session_name", "external_id"])
+
+        return schema
 
     @classmethod
     def enabled(cls):
@@ -81,14 +120,35 @@ class CloudWatch(BaseQueryRunner):
     def test_connection(self):
         self.get_schema()
 
+    def _get_iam_credentials(self, user=None):
+        if ASSUME_ROLE:
+            role_session_name = 'redash' if user is None else user.email
+            sts = boto3.client('sts')
+            creds = sts.assume_role(
+                RoleArn=self.configuration['assume_role_arn'],
+                RoleSessionName=role_session_name,
+                ExternalId=self.configuration.get('external_id')
+            )
+            return {
+                'aws_access_key_id': creds['Credentials']['AccessKeyId'],
+                'aws_secret_access_key': creds['Credentials']['SecretAccessKey'],
+                'aws_session_token': creds['Credentials']['SessionToken'],
+                'region_name': self.configuration.get('region')
+            }
+        else:
+            return {
+                'aws_access_key_id': self.configuration.get('aws_access_key'),
+                'aws_secret_access_key': self.configuration.get('aws_secret_key'),
+                'region_name': self.configuration.get('region')
+            }
+
     def _get_client(self):
-        cloudwatch = boto3.client(
-            "cloudwatch",
-            region_name=self.configuration.get("region"),
-            aws_access_key_id=self.configuration.get("aws_access_key"),
-            aws_secret_access_key=self.configuration.get("aws_secret_key"),
-        )
-        return cloudwatch
+        try:
+            cloudwatch = boto3.client("cloudwatch", **self._get_iam_credentials(self.current_user))
+            return cloudwatch
+        except Exception as e:
+            logger.exception(f"Failed to connect to CloudWatch: {e}")
+            raise
 
     def get_schema(self, get_stats=False):
         client = self._get_client()
